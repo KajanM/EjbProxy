6cef9447366c85a8340b78873a6028227b652478
Nuran Arachchi <nuran.kasthuriarachchi@proavos.com>
2015-05-14 14:14:19 +0530

    Web service example 1

---
+++ webService/src/main/java/com/arachchi/webservice/core/dto/Greeting.java
@@ -0,0 +1,24 @@
+package com.arachchi.webservice.core.dto;
+
+/**
+ * @author nuran
+ * @since 5/13/15
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
